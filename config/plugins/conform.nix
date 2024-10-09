400a8f833676d1e40f28c364d347f7af22116eaa
Mirza Hilmi <dev@mrz.my.id>
2024-10-09 20:12:41 +0700

    refactor(conform): disable c# format on-save

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -17,7 +17,7 @@
         -- Disable "format_on_save lsp_fallback" for lanuages that don't
         -- have a well standardized coding style. You can add additional
         -- lanuages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, cs = true }
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype]
