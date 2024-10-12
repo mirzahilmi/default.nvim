497b94c06513f2d0e9a7e2c358b2ff66b40dcb83
Mirza Hilmi <dev@mrz.my.id>
2024-10-12 18:05:45 +0700

    feat(lsp): add bashls

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -1,28 +1,9 @@
-{pkgs, ...}: {
-  # Dependencies
-  #
-  # https://nix-community.github.io/nixvim/plugins/cmp-nvim-lsp.html
-  plugins.cmp-nvim-lsp = {
-    enable = true;
+{
+  plugins = {
+    cmp-nvim-lsp.enable = true;
+    fidget.enable = true;
+    lspkind.enable = true;
   };
-
-  # Useful status updates for LSP.
-  # https://nix-community.github.io/nixvim/plugins/fidget/index.html
-  plugins.fidget = {
-    enable = true;
-  };
-
-  plugins.lspkind = {
-    enable = true;
-  };
-
-  # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=extraplugi#extraplugins
-  extraPlugins = with pkgs.vimPlugins; [
-    # NOTE: This is how you would ad a vim plugin that is not implemented in Nixvim, also see extraConfigLuaPre below
-    # `neodev` configure Lua LSP for your Neovim config, runtime and plugins
-    # used for completion, annotations, and signatures of Neovim apis
-    neodev-nvim
-  ];
 
   extraConfigLua = ''
     local lspconfig = require("lspconfig")
@@ -49,10 +30,7 @@
     }
   '';
 
-  # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=extraplugi#extraconfigluapre
   extraConfigLuaPre = ''
-    require('neodev').setup {}
-
     vim.diagnostic.config({update_in_insert = true})
     vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
       vim.lsp.handlers.hover,
@@ -60,101 +38,32 @@
     )
   '';
 
-  # https://nix-community.github.io/nixvim/NeovimOptions/autoGroups/index.html
   autoGroups = {
     "kickstart-lsp-attach" = {
       clear = true;
     };
   };
 
-  # Brief aside: **What is LSP?**
-  #
-  # LSP is an initialism you've probably heard, but might not understand what it is.
-  #
-  # LSP stands for Language Server Protocol. It's a protocol that helps editors
-  # and language tooling communicate in a standardized fashion.
-  #
-  # In general, you have a "server" which is some tool built to understand a particular
-  # language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-  # (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-  # processes that communicate with some "client" - in this case, Neovim!
-  #
-  # LSP provides Neovim with features like:
-  #  - Go to definition
-  #  - Find references
-  #  - Autocompletion
-  #  - Symbol Search
-  #  - and more!
-  #
-  # Thus, Language Servers are external tools that must be installed separately from
-  # Neovim which are configured below in the `server` section.
-  #
-  # If you're wondering about lsp vs treesitter, you can check out the wonderfully
-  # and elegantly composed help section, `:help lsp-vs-treesitter`
-  #
-  # https://nix-community.github.io/nixvim/plugins/lsp/index.html
   plugins.lsp = {
     enable = true;
-
-    # Enable the following language servers
-    #  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    #
-    #  Add any additional override configuration in the following tables. Available keys are:
-    #  - cmd: Override the default command used to start the server
-    #  - filetypes: Override the default list of associated filetypes for the server
-    #  - capabilities: Override fields in capabilities. Can be used to disable certain LSP features.
-    #  - settings: Override the default settings passed when initializing the server.
-    #        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-    # servers = {
-    #   phpactor = {
-    #     enable = true;
-    #     package = null;
-    #     cmd = ["${pkgs.phpactor}/bin/phpactor"];
-    #   };
-    #   gopls = {
-    #     enable = true;
-    #     package = null;
-    #     # cmd = ["${pkgs.gopls}/bin/gopls"];
-    #   };
-    #   nixd = {
-    #     enable = true;
-    #     package = null;
-    #     cmd = ["${pkgs.nixd}/bin/nixd"];
-    #     settings.formatting.command = ["${pkgs.alejandra}/bin/alejandra"];
-    #   };
-    #   yamlls = {
-    #     enable = true;
-    #   };
-    #   lua-ls = {
-    #     enable = true;
-    #     settings = {
-    #       completion = {
-    #         callSnippet = "Replace";
-    #       };
-    #     };
-    #   };
-    # };
-
+    servers = {
+      bashls.enable = true;
+    };
     keymaps = {
-      # Diagnostic keymaps
       diagnostic = {
         "[d" = {
-          #mode = "n";
           action = "goto_prev";
           desc = "Go to previous [D]iagnostic message";
         };
         "]d" = {
-          #mode = "n";
           action = "goto_next";
           desc = "Go to next [D]iagnostic message";
         };
         "<leader>e" = {
-          #mode = "n";
           action = "open_float";
           desc = "Show diagnostic [E]rror messages";
         };
         "<leader>q" = {
-          #mode = "n";
           action = "setloclist";
           desc = "Open diagnostic [Q]uickfix list";
         };
@@ -268,52 +177,54 @@
     #   an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
     #   function will be executred to configure the current buffer
     # NOTE: This is an example of an attribute that takes raw lua
-    onAttach = ''
-      -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-      -- to define small helper and utility functions so you don't have to repeat yourself.
-      --
-      -- In this case, we create a function that lets us more easily define mappings specific
-      -- for LSP related items. It sets the mode, buffer and description for us each time.
-      local map = function(keys, func, desc)
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = 'LSP: ' .. desc })
-      end
+    onAttach =
+      #lua
+      ''
+        -- NOTE: Remember that Lua is a real programming language, and as such it is possible
+        -- to define small helper and utility functions so you don't have to repeat yourself.
+        --
+        -- In this case, we create a function that lets us more easily define mappings specific
+        -- for LSP related items. It sets the mode, buffer and description for us each time.
+        local map = function(keys, func, desc)
+          vim.keymap.set('n', keys, func, { buffer = bufnr, desc = 'LSP: ' .. desc })
+        end
 
-      -- The following two autocommands are used to highlight references of the
-      -- word under the cursor when your cursor rests there for a little while.
-      --    See `:help CursorHold` for information about when this is executed
-      --
-      -- When you move your cursor, the highlights will be cleared (the second autocommand).
-      if client and client.server_capabilities.documentHighlightProvider then
-        local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-        vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-          buffer = bufnr,
-          group = highlight_augroup,
-          callback = vim.lsp.buf.document_highlight,
-        })
+        -- The following two autocommands are used to highlight references of the
+        -- word under the cursor when your cursor rests there for a little while.
+        --    See `:help CursorHold` for information about when this is executed
+        --
+        -- When you move your cursor, the highlights will be cleared (the second autocommand).
+        if client and client.server_capabilities.documentHighlightProvider then
+          local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+            buffer = bufnr,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.document_highlight,
+          })
 
-        vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-          buffer = bufnr,
-          group = highlight_augroup,
-          callback = vim.lsp.buf.clear_references,
-        })
+          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+            buffer = bufnr,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.clear_references,
+          })
 
-        vim.api.nvim_create_autocmd('LspDetach', {
-          group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-          callback = function(event2)
-            vim.lsp.buf.clear_references()
-            vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-          end,
-        })
-      end
-      -- The following autocommand is used to enable inlay hints in your
-      -- code, if the language server you are using supports them
-      --
-      -- This may be unwanted, since they displace some of your code
-      if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-        map('<leader>th', function()
-          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-        end, '[T]oggle Inlay [H]ints')
-      end
-    '';
+          vim.api.nvim_create_autocmd('LspDetach', {
+            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+            callback = function(event2)
+              vim.lsp.buf.clear_references()
+              vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+            end,
+          })
+        end
+        -- The following autocommand is used to enable inlay hints in your
+        -- code, if the language server you are using supports them
+        --
+        -- This may be unwanted, since they displace some of your code
+        if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+          map('<leader>th', function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+          end, '[T]oggle Inlay [H]ints')
+        end
+      '';
   };
 }
