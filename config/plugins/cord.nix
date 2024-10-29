9bebe8a603047a822a42b060857b08624b9a2c29
Mirza Hilmi <dev@mrz.my.id>
2024-10-29 18:19:07 +0700

    feat(cord): add hcl file icons

--- config/plugins/cord.nix
+++ config/plugins/cord.nix
@@ -1,22 +1,29 @@
 {pkgs, ...}: {
   extraPlugins = with pkgs.vimPlugins; [cord-nvim];
-  extraConfigLua = ''
-    require('cord').setup {
-      editor = { tooltip = "Neovim" },
-      display = { show_cursor_position = true },
-      lsp = { show_problem_count = false },
-      assets = {
-        [".tf"] = {
-          icon = "https://i.postimg.cc/8C4x6HxS/terraform.png",
-          tooltip = "Terraform",
-          type = 0,
+  extraConfigLua =
+    #Lua
+    ''
+      require('cord').setup {
+        editor = { tooltip = "Neovim" },
+        display = { show_cursor_position = true },
+        lsp = { show_problem_count = false },
+        assets = {
+          ["terraform"] = {
+            icon = "https://i.postimg.cc/8C4x6HxS/terraform.png",
+            tooltip = "Terraform",
+            type = 0,
+          },
+          ["terraform-vars"] = {
+            icon = "https://i.postimg.cc/8C4x6HxS/terraform.png",
+            tooltip = "Terraform",
+            type = 0,
+          },
+          ["hcl"] = {
+            icon = "https://i.postimg.cc/Dw8WSH1w/image.png",
+            tooltip = "HashiCorp Configuration Language",
+            type = 0,
+          }
         },
-        [".tfvars"] = {
-          icon = "https://i.postimg.cc/8C4x6HxS/terraform.png",
-          tooltip = "Terraform",
-          type = 0,
-        }
-      },
-    }
-  '';
+      }
+    '';
 }
