7934ae8574c16fea7ae5b5647e9765d3685baca8
Mirza Hilmi <dev@mrz.my.id>
2024-11-21 19:37:20 +0700

    feat(gopls): add extra option copied from lazyvim

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -7,7 +7,44 @@
 
   extraConfigLua = ''
     local lspconfig = require("lspconfig")
-    lspconfig.gopls.setup {}
+    lspconfig.gopls.setup {
+      settings = {
+        gopls = {
+          gofumpt = true,
+          codelenses = {
+            gc_details = false,
+            generate = true,
+            regenerate_cgo = true,
+            run_govulncheck = true,
+            test = true,
+            tidy = true,
+            upgrade_dependency = true,
+            vendor = true,
+          },
+          hints = {
+            assignVariableTypes = true,
+            compositeLiteralFields = true,
+            compositeLiteralTypes = true,
+            constantValues = true,
+            functionTypeParameters = true,
+            parameterNames = true,
+            rangeVariableTypes = true,
+          },
+          analyses = {
+            fieldalignment = true,
+            nilness = true,
+            unusedparams = true,
+            unusedwrite = true,
+            useany = true,
+          },
+          usePlaceholders = true,
+          completeUnimported = true,
+          staticcheck = true,
+          directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+          semanticTokens = true,
+        },
+      },
+    }
     lspconfig.phpactor.setup {}
     lspconfig.nixd.setup {}
     lspconfig.arduino_language_server.setup {}
@@ -213,6 +250,17 @@
           map('<leader>th', function()
             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
           end, '[T]oggle Inlay [H]ints')
+        end
+
+        -- workaround for gopls not supporting semanticTokensProvider
+        -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+        if client.name == 'gopls' and not client.server_capabilities.semanticTokensProvider then
+          local semantic = client.config.capabilities.textDocument.semanticTokens
+          client.server_capabilities.semanticTokensProvider = {
+            full = true,
+            legend = {tokenModifiers = semantic.tokenModifiers, tokenTypes = semantic.tokenTypes},
+            range = true,
+          }
         end
       '';
   };
