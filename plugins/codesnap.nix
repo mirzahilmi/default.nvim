ecde56b6d9292960fe6e9bc85039b4dc54e5f58b
Mirza Hilmi <dev@mrz.my.id>
2024-11-30 17:38:31 +0700

    refactor(codesnap): remove bg, font to iosevka, others

--- plugins/codesnap.nix
+++ plugins/codesnap.nix
@@ -2,8 +2,13 @@
   plugins.codesnap = {
     enable = true;
     settings = {
+      bg_color = "#FAF4E8";
+      bg_padding = 0;
+      code_font_family = "Iosevka NF";
+      has_breadcrumbs = true;
       has_line_number = true;
-      save_path = "~/Pictures/Screenshots/";
+      mac_window_bar = false;
+      save_path = "~/Pictures/Screenshots";
       watermark = "";
     };
   };
