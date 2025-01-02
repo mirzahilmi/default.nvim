50e1ad4b580f2bd3f480cb6e8ed6883b7141240e
Mirza Hilmi <dev@mrz.my.id>
2025-01-02 21:12:02 +0700

    refactor(nvim-dap): configure .NET adapter correctly

--- plugins/dap.nix
+++ plugins/dap.nix
@@ -4,27 +4,40 @@
     extensions = {
       dap-ui.enable = true;
     };
-    adapters.servers = {
-      coreclr.executable = {
-        command = "netcoredbg";
-        args = ["--interpreter=vscode"];
-      };
-    };
-    configurations = {
-      cs = [
-        {
-          type = "coreclr";
-          name = "launch - netcoredbg";
-          request = "launch";
-          program = ''
-            function()
-                return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
-            end
-          '';
-        }
-      ];
-    };
   };
+  extraConfigLua = ''
+    local dap, dapui = require("dap"), require("dapui")
+
+    dap.adapters.coreclr = {
+      type = "executable",
+      command = "netcoredbg",
+      args = {"--interpreter=vscode"}
+    }
+
+    dap.configurations.cs = {
+      {
+        type = "coreclr",
+        name = "launch - netcoredbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+        end,
+      },
+    }
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+  '';
   keymaps = [
     {
       mode = "n";
